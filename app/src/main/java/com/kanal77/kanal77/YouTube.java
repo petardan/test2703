592ac5616d0884612312591a7d3514a2ffc709b3
Daci <karadzovski.dragan@gmail.com>
2017-03-27 13:33:00 +0200

    Dodaden YouTube  Web View

---
+++ app/src/main/java/com/kanal77/kanal77/YouTube.java
@@ -0,0 +1,41 @@
+package com.kanal77.kanal77;
+
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class YouTube extends Activity {
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_you_tube2);
+
+
+        myWebView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("http://youtube.com/user/RadioMrezaKanal77");
+        myWebView.setWebViewClient(new WebViewClient());
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
+
+
